fe7f29df31ee5d61a1e2840d3078ec66eb2f5cda
Estudiantes <Estudiantes@labing50202.udistrital.edu.co>
2017-03-24 16:39:31 -0500

    1ra media hora

---
+++ Aplicacion/src/aplicacion/Calculadora.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Calculadora {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+        Calculadora cal= new Calculadora();
+        /*
+        miCo.miCalcu=cal;
+        miCo.Conso();
+        */
+        Presentacion miG= new Presentacion();
+        miG.miCalGra=cal;
+        miG.setVisible(true);
+               
+        // TODO code application logic here
+    }
+        // TODO code application logic here
+    }
+    
+
